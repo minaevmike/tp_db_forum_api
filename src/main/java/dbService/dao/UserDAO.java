c6c63da97c24648a7cc1c4640099d141115d194b
anrak <rakhubov@yandex.ru>
2014-04-21 21:55:35 +0400

    Typed Result handler

--- src/main/java/dbService/dao/UserDAO.java
+++ src/main/java/dbService/dao/UserDAO.java
@@ -4,7 +4,9 @@ import dataSets.UserData;
 import dbService.DataService;
 import dbService.executor.SimpleExecutor;
 import dbService.executor.TExecutor;
+import dbService.handlers.TResultHandler;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -19,25 +21,25 @@ public class UserDAO {
         dataService = ds;
     }
 
-//    public UserData getUser(String name) throws SQLException
-//    {
-//        TExecutor exec = new TExecutor();
-//        TResultHandler<User> resultHandler = new TResultHandler<User>(){
-//
-//            public User handle(ResultSet result) throws SQLException {
-//                result.next();
-//                return new User(result.getI(1), result.getString(2), result.getString(3));
-//            }
-//        };
-//        return exec.execQuery(dataService.getConnection(), "SELECT * FROM users WHERE name="
-//                + escape(name), resultHandler);
-//    }
+    public UserData getUserByMail(String userMail) throws SQLException
+    {
+        TExecutor exec = new TExecutor();
+        TResultHandler<UserData> resultHandler = new TResultHandler<UserData>(){
+
+            public UserData handle(ResultSet result) throws SQLException {
+                result.next();
+                return new UserData(result.getInt(1), result.getString(2), result.getString(3), result.getString(4),
+                                        result.getString(5), result.getBoolean(6));
+            }
+        };
+        return exec.execQuery(dataService.getConnection(), "SELECT * FROM Users WHERE mail='" + userMail +"'", resultHandler);
+    }
 
     public void createUser(UserData user) throws SQLException
     {
         SimpleExecutor exec = new SimpleExecutor();
         StringBuilder sb = new StringBuilder();
-        sb.append("INSERT INTO `forum_api`.`Users` (`username`, `mail`, `name`, `isAnonymous`, `about`) VALUES (")
+        sb.append("INSERT INTO Users (`username`, `mail`, `name`, `isAnonymous`, `about`) VALUES (")
                 .append("'").append(user.getUsername()).append("'") .append(",")
                 .append("'").append(user.getMail()).append("'")     .append(",")
                 .append("'").append(user.getName()).append("'")     .append(",")
