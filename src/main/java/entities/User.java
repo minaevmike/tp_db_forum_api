3816bd6ae1b82dd99cee69d0ce4361595df4c256
anrak <rakhubov@yandex.ru>
2014-04-22 02:16:26 +0400

    user details implemented

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -3,26 +3,110 @@ package entities;
 import dataSets.UserData;
 import dataSets.UserParser;
 import dbService.DataService;
-import dbService.dao.UserDAO;
+import dbService.executor.SimpleExecutor;
+import dbService.executor.TExecutor;
+import dbService.handlers.TResultHandler;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Andrey  21.04.14.
  */
 public class User implements TableInterface {
 
-    DataService ds;
-    UserDAO dao;
+    DataService dataService;
 
-    public User(DataService ds)
+    public User(DataService dataService)
     {
-        this.ds = ds;
-        dao = new UserDAO(ds);
+        this.dataService = dataService;
     }
+
+    private UserData getUserByMail(String userMail) throws SQLException
+    {
+        TExecutor exec = new TExecutor();
+        TResultHandler<UserData> resultHandler = new TResultHandler<UserData>(){
+
+            public UserData handle(ResultSet result) throws SQLException {
+                result.next();
+                return new UserData(result.getInt(1), result.getString(2), result.getString(3), result.getString(4),
+                        result.getBoolean(5), result.getString(6));
+            }
+        };
+        return exec.execQuery(dataService.getConnection(), "SELECT * FROM Users WHERE mail='" + userMail +"'", resultHandler);
+    }
+
+    private List<String> getFollowers(int id) throws SQLException {
+        TExecutor exec = new TExecutor();
+        TResultHandler<List<String>> resultHandler = new TResultHandler<List<String>>(){
+
+            public List<String> handle(ResultSet result) throws SQLException {
+                List<String> list= new LinkedList<>();
+                while(result.next()) {
+                    list.add(result.getString(1));
+                }
+                return list;
+            }
+        };
+
+        return exec.execQuery(dataService.getConnection(),
+               "SELECT u.mail FROM Follows f JOIN Users u ON u.id=f.follow WHERE f.user=" + id, resultHandler);
+    }
+
+    private List<String> getFollowing(int id) throws SQLException {
+        TExecutor exec = new TExecutor();
+        TResultHandler<List<String>> resultHandler = new TResultHandler<List<String>>(){
+
+            public List<String> handle(ResultSet result) throws SQLException {
+                List<String> list= new LinkedList<>();
+                while(result.next()) {
+                    list.add(result.getString(1));
+                }
+                return list;
+            }
+        };
+
+        return exec.execQuery(dataService.getConnection(),
+                "SELECT user FROM Follows WHERE follow=" + id, resultHandler);
+    }
+
+    private List<String> getSubscriptions(int id) throws SQLException {
+        TExecutor exec = new TExecutor();
+        TResultHandler<List<String>> resultHandler = new TResultHandler<List<String>>(){
+
+            public List<String> handle(ResultSet result) throws SQLException {
+                List<String> list= new LinkedList<>();
+                while(result.next()) {
+                    list.add(result.getString(1));
+                }
+                return list;
+            }
+        };
+
+        return exec.execQuery(dataService.getConnection(),
+                "SELECT thread_id FROM Subscriptions WHERE user_id=" + id, resultHandler);
+    }
+
+    private void createUser(UserData user) throws SQLException
+    {
+        SimpleExecutor exec = new SimpleExecutor();
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO Users (`username`, `mail`, `name`, `isAnonymous`, `about`) VALUES (")
+                .append("'").append(user.getUsername()).append("'") .append(",")
+                .append("'").append(user.getMail()).append("'")     .append(",")
+                .append("'").append(user.getName()).append("'")     .append(",")
+                            .append(user.isAnonymous())             .append(",")
+                .append("'").append(user.getAbout()).append("'")
+                .append(")");
+
+        exec.execUpdate(dataService.getConnection(), sb.toString());
+    }
+
 
     private String create(String data)
     {
@@ -36,26 +120,14 @@ public class User implements TableInterface {
             }
         }
         try {
-            dao.createUser(userParser.getUserData());
+            createUser(userParser.getUserData());
 
-            UserData created = dao.getUserByMail(userParser.getUserData().getMail());
-            JSONObject obj = new JSONObject();
-            JSONObject resultJson = new JSONObject();
+            UserData created = getUserByMail(userParser.getUserData().getMail());
+            JSONObject userObj = UserDataToJson(created);
+            JSONObject resultJson = createResponse(userObj);
 
-            obj.put("username", created.getUsername());
-            obj.put("about", created.getAbout());
-            obj.put("isAnonymous", created.isAnonymous());
-            obj.put("name", created.getName());
-            obj.put("email", created.getMail());
-            obj.put("id", created.getId());
-
-            resultJson.put("code", 0);
-            resultJson.put("response", obj);
-
-            System.out.print(resultJson.toJSONString());
-
+            System.out.println(resultJson.toJSONString());
             return resultJson.toJSONString();
-
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -67,14 +139,24 @@ public class User implements TableInterface {
     private String details(String query)
     {
         String[] pairs = query.split("=");
-        UserData userData = null;
+        String mail = pairs[1].replaceFirst("%40","@");
         try {
-            userData = dao.getUserByMail(pairs[1]);
+            UserData userData = getUserByMail(mail);
+            List<String> followers = getFollowers(userData.getId());
+            List<String> following = getFollowing(userData.getId());
+            List<String> subscriptions = getSubscriptions(userData.getId());
+
+            JSONObject obj = UserDataToJson(userData);
+            obj.put("following", following);
+            obj.put("followers", followers);
+            obj.put("subscriptions", subscriptions);
+            JSONObject resultJson = createResponse(obj);
+
+            System.out.println(resultJson.toJSONString());
+            return resultJson.toJSONString();
+
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        if(userData != null) {
-
         }
         return null;
     }
@@ -90,4 +172,29 @@ public class User implements TableInterface {
         }
         return null;
     }
+
+    private JSONObject UserDataToJson(UserData user)
+    {
+        JSONObject obj = new JSONObject();
+
+        obj.put("username", user.getUsername());
+        obj.put("about", user.getAbout());
+        obj.put("isAnonymous", user.isAnonymous());
+        obj.put("name", user.getName());
+        obj.put("email", user.getMail());
+        obj.put("id", user.getId());
+
+        return obj;
+    }
+
+    private JSONObject createResponse(JSONObject obj)
+    {
+        JSONObject resultJson = new JSONObject();
+        resultJson.put("code", 0);
+        resultJson.put("response", obj);
+
+        return resultJson;
+    }
+
+
 }
