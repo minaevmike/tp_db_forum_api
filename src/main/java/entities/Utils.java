549cbbe2210add06b093bbc1b25d870772b8fefc
anrak <rakhubov@yandex.ru>
2014-04-22 22:22:02 +0400

    utility - clean database

---
+++ src/main/java/entities/Utils.java
@@ -0,0 +1,65 @@
+package entities;
+
+import dataSets.ForumData;
+import dataSets.parser.ForumParser;
+import dbService.DataService;
+import dbService.executor.SimpleExecutor;
+import dbService.executor.TExecutor;
+import dbService.handlers.TResultHandler;
+import utils.JsonHelper;
+import utils.ValueStringBuilder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Andrey
+ * 22.04.14.
+ */
+public class Utils implements TableInterface {
+
+    DataService dataService;
+
+    public Utils(DataService dataService)
+    {
+        this.dataService = dataService;
+    }
+
+
+    private String clear()
+    {
+        Connection connection = dataService.getConnection();
+        PreparedStatement ups = null;
+        try {
+            ups = connection.prepareStatement("select concat(\'TRUNCATE TABLE \', table_schema" +
+                    ",\'.\',TABLE_NAME,\';\') FROM INFORMATION_SCHEMA.TABLES where table_schema = \'forum_api\';");
+            ResultSet resultSet = ups.executeQuery();
+            connection.setAutoCommit(false);
+            PreparedStatement FKoff = connection.prepareStatement("SET FOREIGN_KEY_CHECKS=0");
+            FKoff.executeUpdate();
+            while (resultSet.next()){
+                PreparedStatement t = connection.prepareStatement(resultSet.getString(1));
+                t.executeUpdate();
+            }
+            PreparedStatement FKon = connection.prepareStatement("SET FOREIGN_KEY_CHECKS=1");
+            FKon.executeUpdate();
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "CLEARED";
+    }
+
+    @Override
+    public String exec(String method, String data) {
+        switch (method) {
+            case "clear":
+                return clear();
+        }
+        return null;
+    }
+}
